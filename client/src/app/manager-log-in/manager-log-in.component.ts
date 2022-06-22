23a4c05cde155de9fc482586802b1ae8fbe91d9d
Aaron <aaronjerez1@gmail.com>
2022-06-22 19:35:39 -0400

    CLIENT: created feature module manager-log-in for the manager functionality

---
+++ client/src/app/manager-log-in/manager-log-in.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manager-log-in',
+  templateUrl: './manager-log-in.component.html',
+  styleUrls: ['./manager-log-in.component.scss']
+})
+export class ManagerLogInComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ client/src/app/manager-log-in/manager-log-in.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ManagerLogInComponent } from './manager-log-in.component';
+
+
+
+@NgModule({
+  declarations: [
+    ManagerLogInComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ManagerLogInModule { }
