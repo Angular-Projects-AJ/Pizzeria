eed824f16382965173446c804e69ad8d7a8bcf44
Aaron <aaronjerez1@gmail.com>
2022-06-22 14:51:44 -0400

    CLIENT: refactoring food.component.html

--- client/src/app/food/food.module.ts
+++ client/src/app/food/food.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FoodComponent } from './food.component';
+import { ProductItemComponent } from './product-item/product-item.component';
 
 
 
 @NgModule({
   declarations: [
-    FoodComponent
+    FoodComponent,
+    ProductItemComponent
   ],
   imports: [
     CommonModule
