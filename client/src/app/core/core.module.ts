75cccfd03798dc37e62ee0fd9d6882ff2b15e85b
Aaron <aaronjerez1@gmail.com>
2022-06-29 07:59:24 -0400

    CLIENT: added user error notifications

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -6,6 +6,7 @@ import { SectionHeaderComponent } from './section-header/section-header.componen
 import { TestErrorComponent } from './test-error/test-error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
+import { ToastrModule } from 'ngx-toastr';
 
 
 
@@ -13,7 +14,11 @@ import { ServerErrorComponent } from './server-error/server-error.component';
   declarations: [NavBarComponent, SectionHeaderComponent, TestErrorComponent, NotFoundComponent, ServerErrorComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true
+    })
   ],
   exports: [NavBarComponent, SectionHeaderComponent]
 })
