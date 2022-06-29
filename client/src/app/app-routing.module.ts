f6313e8f796ef4314a68a07804b900148187d891
Aaron <aaronjerez1@gmail.com>
2022-06-29 15:34:10 -0400

    CLIENT: Added the account service methods to register, log in and log out

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -21,6 +21,7 @@ const routes: Routes = [
   {path: 'food', loadChildren: () => import('./food/food.module').then(mod => mod.FoodModule)},
   {path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule)},
   {path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then(mod => mod.CheckoutModule)},
+  {path: 'account', loadChildren: () => import('./account/account.module').then(mod => mod.AccountModule)},
   {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
